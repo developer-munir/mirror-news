ac9f1dc92d3a9220be53fc12ed4179eab6e6ae2d
developer-munir <devolopermunir@gmail.com>
2022-09-02 22:18:10 +0600

    catagories links added dynamic with api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("main");
+/* ---------------------------
+    categories section 
+------------------------------ */
+const categoriesData = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => categories(data.data.news_category))
+      .catch((error) => console.log(error));
+}
+const categories = datas => {
+    const categoryLinkContainer = document.getElementById("categories-links");
+    datas.forEach(data => {
+        const categoryName = data.category_name;
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <a class="nav-link" href="#">${categoryName}</a>
+        `;
+        categoryLinkContainer.appendChild(li);
+    });
+}
+categoriesData();
--- js/utilites.js
+++ js/utilites.js
@@ -1,1 +1,1 @@
-console.log('utilites');
+// console.log('utilites');
